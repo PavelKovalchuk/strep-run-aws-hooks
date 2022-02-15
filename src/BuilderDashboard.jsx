29fd8d402dd5a63f4e1ab333c6a99048ef0d83c4
pkovalchuk <pavel.kovalchuk@dataart.com>
2022-02-15 14:48:22 +0200

    base structure for Get Last Time Build

--- src/BuilderDashboard.jsx
+++ src/BuilderDashboard.jsx
@@ -7,20 +7,13 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Button from '@mui/material/Button';
 import Alert from '@mui/material/Alert';
+import Grid from '@mui/material/Grid';
 
 import Flags from 'country-flag-icons/react/3x2';
 
 import './BuilderDashboard.css';
+import { compareTitles } from './helpers';
 
-function compareTitles( a, b ) {
-  if ( a.title < b.title ){
-    return -1;
-  }
-  if ( a.title > b.title ){
-    return 1;
-  }
-  return 0;
-}
 
 function BuilderDashboard({variants, id}) {
   const [locale, setLocale] = useState('');
@@ -70,6 +63,47 @@ function BuilderDashboard({variants, id}) {
     }
   };
 
+  const handleLastTimeBuild = async () => {
+    const data = variants.find((item) => {
+      return item.value === locale;
+    });
+
+    if(!data) {
+      setMessage({text: "No such locale was found", type: "error"});
+      return;
+    }
+
+    /* if(!data.hook) {
+      setMessage({text: "No such hook found", type: "error"});
+      return;
+    } */
+
+    // const cleanHook = data.hook.replace('"', '').replace('\\', '');
+
+
+
+    try {
+      const result = await fetch("https://develop.d1tmxp48xm1j0i.amplifyapp.com/", {
+        method: 'GET',
+        mode: 'no-cors',
+        // credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'text/html'
+        }
+      }).then((data) => {
+        console.log("data", data);
+      });
+
+      console.log("result", result);
+
+      setLocale("");
+      setMessage({text: "Success", type: "success"});
+
+    } catch (error) {
+      setMessage({text: "Error: " + error, type: "error"});
+    }
+  };
+
   variants.sort( compareTitles );
 
   return (
@@ -81,29 +115,54 @@ function BuilderDashboard({variants, id}) {
       }
       
       <FormControl fullWidth>
-        <InputLabel id={`${id}-InputLabel`}>{id}</InputLabel>
-        <Select
-          labelId={`${id}-InputLabel`}
-          id={`${id}-Select`}
-          value={locale}
-          label={`${id}`}
-          onChange={handleLocaleChange}
-        >
-          {variants.map((item) => {
-            const Flag = item?.flagCode ? Flags[item.flagCode.toUpperCase()] : null;
-
-            return (
-              <MenuItem 
-                key={item.title} 
-                value={item.value}
+        
+        <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <InputLabel id={`${id}-InputLabel`}>{id}</InputLabel>
+              <Select
+                labelId={`${id}-InputLabel`}
+                id={`${id}-Select`}
+                value={locale}
+                label={`${id}`}
+                onChange={handleLocaleChange}
               >
-              {item.title}
-              {Flag ? <Flag title={item.title} /> : null}
-            </MenuItem>
-            );
-          })}
-        </Select>
-        <Button variant="contained" disabled={!locale} onClick={handleBuild}>Build</Button>
+                {variants.map((item) => {
+                  const Flag = item?.flagCode ? Flags[item.flagCode.toUpperCase()] : null;
+
+                  return (
+                    <MenuItem 
+                      key={item.title} 
+                      value={item.value}
+                    >
+                    {item.title}
+                    {Flag ? <Flag title={item.title} /> : null}
+                  </MenuItem>
+                  );
+                })}
+              </Select>
+            </Grid>
+            <Grid item xs={12}>
+              <Button 
+                variant="contained" 
+                disabled={!locale} 
+                onClick={handleBuild}
+                size="large"
+              >
+                Build
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <Button 
+                variant="outlined" 
+                color="secondary" 
+                disabled={!locale} 
+                onClick={handleLastTimeBuild}
+                size="large"
+              >
+                Get Last Time Build
+              </Button>
+            </Grid>
+          </Grid>
       </FormControl>
     </Box>
   );
