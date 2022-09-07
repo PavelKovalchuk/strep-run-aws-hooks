90dd42fb125696347495adc7a81aa021e8c2c5da
pkovalchuk <pavel.kovalchuk@dataart.com>
2022-09-07 11:36:25 +0200

    add prod Finland and Norway

--- src/constants/prod.js
+++ src/constants/prod.js
@@ -44,4 +44,20 @@ export const ProdVariants = [
     hook: process.env.REACT_APP_PROD_SWEDEN,
     awsCode: "dahnr38ichbbk",
   },
+  {
+    title: "Finland",
+    value: "Finland",
+    flagCode: "fi",
+    webLink: "https://master.d2ujsrnhwms4ep.amplifyapp.com/",
+    hook: process.env.REACT_APP_PROD_FINLAND,
+    awsCode: "d2ujsrnhwms4ep",
+  },
+  {
+    title: "Norway",
+    value: "Norway",
+    flagCode: "no",
+    webLink: "https://master.d3qrime9idzajg.amplifyapp.com/",
+    hook: process.env.REACT_APP_PROD_NORWAY,
+    awsCode: "d3qrime9idzajg",
+  },
 ];
